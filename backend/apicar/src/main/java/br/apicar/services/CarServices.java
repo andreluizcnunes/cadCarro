bab0736a0864ac79249733b8ae3811194ee307c3
André Luiz <andre.doctors@gmail.com>
2023-07-17 16:31:55 -0300

    list CarMinDTO

--- backend/apicar/src/main/java/br/apicar/services/CarServices.java
+++ backend/apicar/src/main/java/br/apicar/services/CarServices.java
@@ -1,5 +1,7 @@
 package br.apicar.services;
 
+import br.apicar.dto.CarMinDTO;
+import br.apicar.models.CarModel;
 import br.apicar.repositories.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,17 @@ import java.util.List;
 
 @Service
 public class CarServices {
+
+    @Autowired
+    CarRepository carRepository;
+
+    public CarServices(CarRepository carRepository){
+        this.carRepository = carRepository;
+    }
+
+    // listar carros DTO cadastrados
+    public List<CarMinDTO> listCarDTO(){
+        List<CarModel> car = carRepository.findAll();
+        return car.stream().map(x -> new CarMinDTO(x)).toList();
+    }
 }
