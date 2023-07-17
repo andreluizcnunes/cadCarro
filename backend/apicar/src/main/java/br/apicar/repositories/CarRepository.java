860bfcc341beaae9645a3ce0d7a7c74191ff3242
André Luiz <andre.doctors@gmail.com>
2023-07-17 16:03:06 -0300

    repository configuration

--- backend/apicar/src/main/java/br/apicar/repositories/CarRepository.java
+++ backend/apicar/src/main/java/br/apicar/repositories/CarRepository.java
@@ -1,4 +1,11 @@
 package br.apicar.repositories;
 
-public interface CarRepository {
+import br.apicar.models.CarModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface CarRepository extends JpaRepository<CarModel, Long> {
 }
