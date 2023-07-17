8ead0f827d0707360dae40f431ed45cf9d0ba88c
André Luiz <andre.doctors@gmail.com>
2023-07-17 16:11:31 -0300

    carResponse method configuration

--- backend/apicar/src/main/java/br/apicar/response/CarResponse.java
+++ backend/apicar/src/main/java/br/apicar/response/CarResponse.java
@@ -1,4 +1,13 @@
 package br.apicar.response;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
 public class CarResponse {
+
+    private String smsResponse;
 }
