97a9b5065990ae3f0f679173dcc2c3a35c47410c
André Luiz <andre.doctors@gmail.com>
2023-07-17 14:55:39 -0300

    created project backend

---
+++ backend/apicar/src/main/java/br/apicar/ApicarApplication.java
@@ -0,0 +1,13 @@
+package br.apicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicarApplication.class, args);
+	}
+
+}
