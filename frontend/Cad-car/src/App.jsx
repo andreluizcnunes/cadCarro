ae576678994877deb5dff63c28afe984111afdd1
André Luiz <andre.doctors@gmail.com>
2023-07-19 16:10:36 -0300

    frontend: get input form

--- frontend/Cad-car/src/App.jsx
+++ frontend/Cad-car/src/App.jsx
@@ -3,9 +3,21 @@ import Formulario from './components/Formulario'
 import Tabela from './components/Tabela'
 
 function App() {
+
+  const carro = {
+    id : 0,
+    fabricante: '',
+    modelo: '',
+    ano: 0,
+    tipoCombustivel: '',
+    motor: '',
+    potencia: '',
+    transmissao: ''
+  }
   
   const [btnCadastrar, setBtnCadastrar] = useState(true);
   const [carros, setCarros] = useState([]);
+  const [objCarro, setObjCarro] = useState(carro);
 
   useEffect(() => {
     fetch("http://localhost:8080/listar")
@@ -13,10 +25,16 @@ function App() {
     .then(retornoConvertido => setCarros(retornoConvertido));
   }, []);
 
+  const getInputForm = (e) =>{
+    setObjCarro({...objCarro, [e.target.name]:e.target.value});
+  }
+
   return (
     <div className='container'>
+      <p>{JSON.stringify(objCarro)}</p>
       <Formulario 
         botao={btnCadastrar}
+        getDadosForm={getInputForm}
       />
       <Tabela
         vetor={carros}
--- frontend/Cad-car/src/components/Formulario.jsx
+++ frontend/Cad-car/src/components/Formulario.jsx
@@ -1,43 +1,85 @@
 import React from "react";
 
-const Formulario = ({ botao }) => {
+const Formulario = ({ botao, getDadosForm }) => {
     return (
         <form className="row g-3">
             <h2>Cadastro de Carro</h2>
 
             <div className="col-md-6">
             <label htmlFor="fabricante" className="form-label">Fabricante:</label>
-                <input type="text" id="fabricante" name="fabricante" className="form-control" />
+                <input 
+                    type="text" 
+                    id="fabricante" 
+                    name="fabricante" 
+                    className="form-control" 
+                    onChange={getDadosForm}
+                />
             </div>
 
             <div className="col-md-6">
                 <label htmlFor="modelo" className="form-label">Modelo:</label>
-                <input type="text" id="modelo" name="modelo" className="form-control" />
+                <input 
+                    type="text" 
+                    id="modelo" 
+                    name="modelo" 
+                    className="form-control" 
+                    onChange={getDadosForm}
+                />
             </div>
 
             <div className="col-md-6">
                 <label htmlFor="ano" className="form-label">Ano:</label>
-                <input type="number" id="ano" name="ano" className="form-control" />
+                <input 
+                    type="number" 
+                    id="ano" 
+                    name="ano" 
+                    className="form-control" 
+                    onChange={getDadosForm}
+                />
             </div>
 
             <div className="col-md-6">
                 <label htmlFor="tipoCombustivel" className="form-label">Tipo Combustivel:</label>
-                <input type="text" id="tipoCombustivel" name="tipoCombustivel" className="form-control" />
+                <input 
+                    type="text" 
+                    id="tipoCombustivel" 
+                    name="tipoCombustivel" 
+                    className="form-control" 
+                    onChange={getDadosForm}
+                />
             </div>
 
             <div className="col-md-4">
                 <label htmlFor="motor" className="form-label">Motor:</label>
-                <input type="text" id="motor" name="motor" className="form-control" />
+                <input 
+                    type="text" 
+                    id="motor" 
+                    name="motor" 
+                    className="form-control" 
+                    onChange={getDadosForm}
+                />
             </div>
 
             <div className="col-md-4">
                 <label htmlFor="potencia" className="form-label">Potencia:</label>
-                <input type="text" id="potencia" name="potencia" className="form-control" />
+                <input 
+                    type="text" 
+                    id="potencia" 
+                    name="potencia" 
+                    className="form-control" 
+                    onChange={getDadosForm}
+                />
             </div>
 
             <div className="col-md-4">
                 <label htmlFor="transmissao" className="form-label">Transmissao:</label>
-                <input type="text" id="transmissao" name="transmissao" className="form-control" />
+                <input 
+                    type="text" 
+                    id="transmissao" 
+                    name="transmissao" 
+                    className="form-control"
+                    onChange={getDadosForm}
+                />
             </div>
             
             {
